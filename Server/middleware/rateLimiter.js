dbd68451eca926ea18aa8a31ae2f463ceb3b4665
Sainath Patil <pspatilsai64@gmail.com>
2025-05-16 02:09:26 +0530

    -c feat Added Rate limit APIs per user & notification feature

---
+++ Server/middleware/rateLimiter.js
@@ -0,0 +1,23 @@
+const rateLimit = require("express-rate-limit");
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each user to 100 requests per windowMs
+
+  // windowMs: 60 * 1000, // 1 minutes
+  // max: 10, // limit each user to 10 requests per windowMs
+
+  keyGenerator: (req) => {
+    if (req.user && req.user._id) {
+      return req.user._id.toString(); // Rate limit per user
+    }
+    return req.ip; // fallback for unauthenticated routes
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    message: "Too many requests. Please try again later.",
+  },
+});
+
+module.exports = apiLimiter;
