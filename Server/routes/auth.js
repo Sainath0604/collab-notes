1fb443309e582214dab7da3ffef609fc2f0c9121
Sainath Patil <pspatilsai64@gmail.com>
2025-05-17 14:13:10 +0530

    feat(COMPLETED): Added get-users API and updated authentication #2

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,12 +1,16 @@
-// routes/auth.js
 const express = require("express");
 const router = express.Router();
-const { signup, login, logout } = require("../controllers/authController");
+const {
+  signup,
+  login,
+  logout,
+  getUsers,
+} = require("../controllers/authController");
 const checkBlacklistedToken = require("../middleware/auth");
 
 router.post("/signup", signup);
 router.post("/login", login);
-
 router.post("/logout", checkBlacklistedToken, logout);
+router.get("/users", getUsers);
 
 module.exports = router;
